532029db6630970438a57b162596dcd96504e316
dimasiksergeevi4 <120562028+dimasiksergeevi4@users.noreply.github.com>
2023-02-18 17:29:16 +0300

    Update settings.py

--- Pereval/settings.py
+++ Pereval/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6b^tr+4x=zcp%$op54!tz887c@#bd%(xcrf9t+)oq+wudsgdfh'
+SECRET_KEY = os.getenv('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
